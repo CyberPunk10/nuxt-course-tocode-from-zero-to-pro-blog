d146cc759d61c556543f0ba4ad22da1e00786a38
CyberPunk10 <konstantin21_arx@gmail.com>
2020-12-05 13:46:59 +0300

    Sass one love

---
+++ components/system/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <div class="navbar">
+      <div class="container">
+        <div class="navbar-content">
+          <div class="logo">Blog</div>
+          <ul class="navbar-list">
+            <li
+              class="navbar-item"
+              v-for="link in links" :key="link.title"
+            >
+              <nuxt-link
+                class="navbar-link"
+                :to="link.url"
+                :title="link.title"
+              >{{ link.title }}</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {title: 'Home', url: '/'},
+        {title: 'About', url: '/about'}
+      ]
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="sass">
+.navbar-item
+  .nuxt-link-exact-active
+    color: #4b4
+
+</style>
