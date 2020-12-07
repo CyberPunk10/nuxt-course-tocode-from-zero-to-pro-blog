16bdd6739e82d491a12d0a14a5c01c073b01fcc1
CyberPunk10 <konstantin21_arx@gmail.com>
2020-12-07 20:01:36 +0300

    -

--- store/index.js
+++ store/index.js
@@ -3,15 +3,31 @@ export const state = () => ({
 })
 
 export const mutations = {
+  setPosts(state, posts) {
+    console.log('[POSTS]', posts)
+    // state.postsLoaded = posts
+    console.log(state.postLoaded)
+  },
+
   addPost(state, post) {
-    console.log(post)
     state.postsLoaded.push(post)
   }
 }
 
 export const actions = {
+  nuxtServerInit({commit}, context) {
+    return this.$axios.get('https://nuxt-course-tocode-blog-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .then(res => {
+        const postsArray = []
+        for (let key in res.data) {
+          postsArray.push({ ...res.data[key], id: key })
+        }
+        commit('setPosts', postsArray)
+      })
+      .catch(e => console.log(e))
+  },
+
   addPost({commit}, post) {
-    console.log(post)
     return this.$axios.post('https://nuxt-course-tocode-blog-default-rtdb.europe-west1.firebasedatabase.app/posts.json', post)
       .then(res => {
         commit('addPost', {...post, id: res.data.name})
@@ -21,5 +37,7 @@ export const actions = {
 }
 
 export const getters = {
-
+  getPostsLoaded(state) {
+    return state.postLoaded
+  }
 }
